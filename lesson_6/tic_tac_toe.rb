1dd519843fa00aaacbf2c6d9509d9fe34e10c478
Hyunjoo Jun <hjj102088@gmail.com>
2022-06-03 13:16:21 -0400

    Added joinor method

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -37,10 +39,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(list, sign=', ', word='or')
+  case list.size
+  when 0 then ''
+  when 1 then list.first
+  when 2 then list.join(" #{word} ")
+  else
+    list[-1] = "#{word} #{list.last}"
+    list.join(sign)
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a position to place a piece: #{joinor(empty_squares(brd))}"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
